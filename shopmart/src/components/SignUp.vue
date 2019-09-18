4b14bb8fc79672e312f303464d5bd0c76c844761
Ankur Gupta <agupta@mail.uni-paderborn.de>
2019-09-18 16:43:53 +0200

    cards at home page

--- shopmart/src/components/SignUp.vue
+++ shopmart/src/components/SignUp.vue
@@ -1,9 +1,56 @@
 <template>
 
+    <v-container class="pa-4 text-center">
+      <v-row class="fill-height" align="center" justify="center">
+        <template v-for="(item, i) in items">
+          <v-col
+            :key="i"
+            cols="12"
+            md="4"
+          >
+            <v-hover v-slot:default="{ hover }">
+              <v-card
+                :elevation="hover ? 12 : 2"
+                :class="{ 'on-hover': hover }"
+              >
+                hello
+              </v-card>
+            </v-hover>
+          </v-col>
+        </template>
+      </v-row>
+    </v-container>
+   
 </template>
+
 
 <script>
 export default {
+
+    data(){
+        return {
+             items: [
+      {
+        title: 'New Releases',
+        text: "It's New Release Friday",
+        subtext: 'Newly released songs. Updated daily.',
+        img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg',
+      },
+      {
+        title: 'Rock',
+        text: 'Greatest Rock Hits',
+        subtext: 'Lose yourself in rock tunes.',
+        img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+      },
+      {
+        title: 'Mellow Moods',
+        text: 'Ambient Bass',
+        subtext: 'Chill beats to mellow you out.',
+        img: 'http://lorempixel.com/output/abstract-q-c-640-480-6.jpg',
+      },
+    ]
+        }
+    }
 
 }
 </script>
--- shopmart/src/views/Home.vue
+++ shopmart/src/views/Home.vue
@@ -1,26 +1,57 @@
 <template>
-<v-carousel :cycle="cycle" interval="3000"> 
-    <v-carousel-item  v-for="deals in dailydeals" :key="deals.id" :src="deals.imageUrl" style="cursor:pointer">
-       <div class="title">{{deals.title}}</div>
-    </v-carousel-item>
-</v-carousel>
+  <div>
+    <v-carousel :cycle="cycle" interval="3000">
+      <v-carousel-item
+        v-for="deals in dailydeals"
+        :key="deals.id"
+        :src="deals.imageUrl"
+        style="cursor:pointer"
+      >
+        <div class="title">{{deals.title}}</div>
+      </v-carousel-item>
+    </v-carousel>
+
+    <v-container class="pa-4 text-center">
+      <v-row class="fill-height" align="center" justify="center">
+        <template v-for="(item, i) in homecards">
+          <v-col :key="i" cols="12" md="4">
+            <v-hover v-slot:default="{ hover }">
+              <v-card :elevation="hover? 12: 2" :class="{'on-hover': hover}">
+                <v-img :src="item.url"
+                height="395px"> 
+                
+                </v-img>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </template>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-
 export default {
-  data(){
+  data() {
     return {
-      cycle: true
-    }
+      cycle: true,
+      homecards: [
+        { title: "clothes", url: "https://i.postimg.cc/59SwJ43k/tshirt.jpg" },
+        { title: "shoes", url: "https://i.postimg.cc/8c2fTY7D/shoes.jpg" },
+        { title: "toys", url: "https://i.postimg.cc/0233Y0wS/toys.jpg" },
+        { title: "electronics", url: "https://i.postimg.cc/Ls6gTLft/phone.jpg" },
+        { title: "books", url: "https://i.postimg.cc/TPvvvvfQ/books.jpg" },
+        { title: "games", url: "https://i.postimg.cc/43QqCK8N/games.jpg" }
+      ]
+    };
   },
-  computed: {    dailydeals () {
-      console.log(this.$store.getters.loadedDeals)
-      return this.$store.getters.loadedDeals
+  computed: {
+    dailydeals() {
+      console.log(this.$store.getters.loadedDeals);
+      return this.$store.getters.loadedDeals;
     }
-
   }
-}
+};
 </script>
 
 <style scoped>
@@ -33,4 +64,12 @@ export default {
   padding: 20px;
   text-align: center;
 }
+
+.v-card {
+transition: opacity .4s ease-in-out;
+}
+
+.v-card:not(.on-hover) {
+opacity: 0.6;
+ }
 </style>
